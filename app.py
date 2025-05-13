5ca3e243b1f740b0dd3e5df591654a5f9b8bb7c3
Eric Kampman <erickampman@yahoo.com>
2025-05-13 08:26:05 -0700

    Update app.py
    Add logging.

--- app.py
+++ app.py
@@ -1,25 +1,28 @@
 from flask import Flask, request, Response
+import logging
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
 
 @app.route("/outbound", methods=["GET", "POST"])
 def outbound():
     to_number = request.values.get("To", None)
+    logging.info(f"Received request with To={to_number}")
 
     if not to_number:
-        return Response(
-            "<Response><Say>No destination number provided</Say></Response>",
-            mimetype="text/xml"
-        )
+        msg = "<Response><Say>No destination number provided</Say></Response>"
+        logging.info(f"Responding with: {msg}")
+        return Response(msg.strip(), mimetype="text/xml")
 
-    twiml = twiml = f"""
-    <?xml version="1.0" encoding="UTF-8"?>
-    <Response>
-        <Dial callerId="+18885974354">
-            <Number>{to_number}</Number>
-        </Dial>
-    </Response>
-    """.strip()
+    twiml = f"""<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Dial callerId="+18885974354">
+    <Number>{to_number}</Number>
+  </Dial>
+</Response>
+""".strip()
+
+    logging.info(f"Responding with: {twiml}")
     return Response(twiml, mimetype="text/xml")
 
 @app.route("/")
@@ -28,4 +31,3 @@ def index():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
