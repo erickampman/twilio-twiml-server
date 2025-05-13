fa19ecebae5d6ec08491824633e5ced7348428cb
Eric Kampman <erickampman@yahoo.com>
2025-05-13 08:57:34 -0700

    Update app.py
    More response fix attempts.

--- app.py
+++ app.py
@@ -20,7 +20,11 @@ def outbound():
     twiml = f"""<?xml version="1.0" encoding="UTF-8"?><Response><Dial callerId="+18885974354"><Number>{to_number}</Number></Dial></Response>"""
 
     logging.info(f"Responding with: {twiml}\n")
-    return Response(clean_twiml(twiml), content_type="application/xml; charset=utf-8")
+    return Response(
+        twiml.encode("utf-8"),
+        headers={"Content-Type": "application/xml; charset=utf-8"}
+    )
+
 
 @app.route("/")
 def index():
